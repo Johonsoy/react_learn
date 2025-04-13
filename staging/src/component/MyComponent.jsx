a49f190b800171f8a2eedf905821c585952da2b2
johnsoy <zzzzhaoziye@gmail.com>
2025-04-13 21:39:55 +0800

    feat: add staging

---
+++ staging/src/component/MyComponent.jsx
@@ -0,0 +1,15 @@
+import {Component} from "react";
+
+class MyComponent extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Hello react!</h1>
+            </div>
+        )
+    }
+
+}
+
+export default MyComponent;
